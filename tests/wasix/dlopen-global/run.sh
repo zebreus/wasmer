b96d463657a35b20ea0fc460ce6de50d41057a74
Zebreus <zebreus@zebre.us>
2025-02-06 20:31:35 +0100

    Improve dlopen test

--- tests/wasix/dlopen-global/run.sh
+++ tests/wasix/dlopen-global/run.sh
@@ -3,27 +3,20 @@
 CFLAGS="\
 --target=wasm32-wasi \
 --sysroot=$WASIX_SYSROOT \
--fPIC"
+-fPIC \
+-nostartfiles"
 
 LDFLAGS="\
 -Wl,--no-entry \
--nostartfiles \
 -Wl,--import-memory \
--Wl,--shared-memory \
--Wl,--max-memory=4294967296 \
+-Wl,--export-all \
 -Wl,--export-dynamic \
--Wl,--export=__heap_base \
--Wl,--export=__stack_pointer \
--Wl,--export=__data_end \
--Wl,--export=__wasm_init_tls \
--Wl,--export=__wasm_signal \
--Wl,--export=__tls_size \
--Wl,--export=__tls_align \
--Wl,--export=__tls_base \
--lwasi-emulated-mman"
+-Wl,--shared-memory \
+-Xlinker --extra-features=bulk-memory,atomics \
+-Wl,--shared"
 
 $CC $CFLAGS $LDFLAGS -o side.wasm side.c
 
 $WASMER -q run main.wasm --mapdir=/code:. >output
 
-printf "0" | diff -u output - 1>/dev/null
+printf "handle: 0x5\nfoo = 42\n" | diff -u output - 1>/dev/null
--- tests/wasix/dlopen-global/side.c
+++ tests/wasix/dlopen-global/side.c
@@ -2,3 +2,12 @@
 #include <stdlib.h>
 
 int foo = 42;
+
+// // Suggestion: implement this feature after dlsym/dlopen works (2)
+// void my_atexitt() { puts("side module atexit"); }
+
+// // Suggestion: implement this feature after dlsym/dlopen works (1)
+// __attribute__((constructor)) static void ctor() {
+//   puts("side module ctor");
+//   atexit(my_atexitt);
+// }
